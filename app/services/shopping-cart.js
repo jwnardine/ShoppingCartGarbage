d2238bdd6a5d16162e6813a75a3d86e85e557c51
Chris Houghton and Jon Nardine <Guest@epicodus-33.local>
2016-04-07 15:25:48 -0700

    get shopping cart total to work

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -5,5 +5,19 @@ export default Ember.Service.extend({
 
   add(item) {
     this.get('items').pushObject(item);
-  }
+  },
+  remove(item) {
+    this.get('items').removeObject(item);
+  },
+  includes(item) {
+    return this.get('item').includes(item)
+  },
+
+  total: Ember.computed('items.[]', function(){
+    var allCost = 0;
+    for (var i = 0; i < this.get('items.length'); i++) {
+      allCost += this.get('items')[i].get('cost');
+    }
+    return allCost;
+  })
 });
